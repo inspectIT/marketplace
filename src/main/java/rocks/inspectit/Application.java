d7adbe4ba9324e6f22db50931f7d941f53f4ff3e
Nikita Kolytschew <nikita.kolytschew@novatec-gmbh.de>
2016-09-11 21:02:25 +0200

    INSPECTIT-2199 Create inspectIT marketplace prototype
    * adding PMD Plugin

--- src/main/java/rocks/inspectit/Application.java
+++ src/main/java/rocks/inspectit/Application.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * Use {@link SuppressWarnings} to suppress all PMD warnings, since this entry-class will trigger a lot of false positive warnings.
+ *
  * @author Nikita Kolytschew
  * @version %I%, %G%
  * @since 1.0.0-SNAPSHOT
  */
+@SuppressWarnings("PMD")
 @SpringBootApplication
 public class Application {
+
+	/**
+	 * Spring-Boot entry point.
+	 *
+	 * @param args arguments on startup. Usually empty.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
