e079d3632c5f5b405003507f8180dceae38e943a
Nikita Kolytschew <nikita.kolytschew@novatec-gmbh.de>
2017-08-29 17:17:19 +0200

    MAR-14 Netflix Eureka for Service Discovery https://inspectit-performance.atlassian.net/browse/MAR-14
    * update spring-cloud dependencies
    * use bootstrap.yml to set application name for cloud-config
    * create simple test-case

--- backend/marketplace.eureka.server/src/test/java/inspectit/rocks/marketplace/eureka/server/ApplicationTests.java
+++ backend/marketplace.eureka.server/src/test/java/inspectit/rocks/marketplace/eureka/server/ApplicationTests.java
@@ -2,15 +2,28 @@ package inspectit.rocks.marketplace.eureka.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void catalogLoads() {
+		ResponseEntity<Map> entity = testRestTemplate.getForEntity("/eureka/apps", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
 }
